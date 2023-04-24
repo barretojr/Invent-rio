4a411c634be65d2ff5b2261cdd8258a928f4b4c7
JONATHAN BARRETO <rodriguesb.jon@gmail.com>
2023-04-24 15:04:35 -0300

    tentativa de correções na rota de cadastro de usuários e login

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyparser.urlencoded({extended:true}));
 
+//carregando bodyparser com json 
+app.use(bodyparser.json());
+
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
 //render pagina inicial
 app.get("/", (req,res)=>{
   res.render("../public/home");
@@ -44,11 +53,37 @@ app.post('/login', async (req, res) => {
     nome: user[0].nome,
     nomeUsuario: user[0].nomeUsuario
   };
-  res.redirect('/dashboard');
+  res.redirect('/home');
 });
 
+//função que faz a autenticação de login
+async function verificaAutenticacao(req, res, next) {
+  const connection = await db.connect();
+  if (req.isAuthenticated()) {
+    // Obtém a área protegida pela rota atual
+    const areaProtegida = req.path.substring(1);
+
+    // Verifica se o usuário tem permissão para acessar a área protegida
+    connection.execute('SELECT * FROM permissoes WHERE usuario_id = ? AND area = ?', [req.user.id, areaProtegida], (err, result) => {
+      if (err) {
+        // Se ocorrer um erro ao consultar as permissões, redireciona o usuário para uma página de erro
+        res.redirect('/erro');
+      } else if (result.length > 0) {
+        // Se o usuário tiver permissão para acessar a área protegida, passe a requisição adiante
+        return next();
+      } else {
+        // Se o usuário não tiver permissão para acessar a área protegida, redireciona-o para a página de login
+        res.redirect('/login');
+      }
+    });
+  } else {
+    // Se o usuário não estiver autenticado, redireciona-o para a página de login
+    res.redirect('/login');
+  }
+}
+
 //inicio select no banco
-app.get('/inventario/', async (req, res) => {
+app.get('/inventario', verificaAutenticacao, async (req, res) => {
     const listagem = await db.showInventario();
     res.render('../views/inventario', {
         listagem: listagem,
@@ -65,53 +100,65 @@ app.listen(port, () => {
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/private'));
 
-
-
-
 //rotas das funções
+
 // Rota para cadastrar um usuário
+
+app.get("/cadastro", (req, res)=>{
+  res.sendFile(__dirname + "/views/adm/cadastro.html")
+})
+
+
+//===========================================================================================================
 app.post('/cadastro', async (req, res) => {
+  console.log('post iniciado')
   const connection = await db.connect();
-  const nome = req.body.nome;
-  const email = req.body.email;
+  const nome = req.body.nome;  
+  const email = req.body.email;  
   const senha = req.body.senha;
   const nomeUsuario = req.body.nomeUsuario;
-
-  // Verifica se todas as informações foram enviadas
+  console.log('carregada as constantes')
+  //Verifica se todas as informações foram enviadas
   if (!nome || !email || !senha || !nomeUsuario) {
+    console.log('preencha os campos')
     return res.status(400).send('Por favor, preencha todos os campos');
   }
-  // Verifica se o e-mail já foi cadastrado
-  db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, results) => {
-    if (err) {
-      return res.status(500).send('Erro ao verificar o e-mail no banco de dados');
-    }
-    if (results.length > 0) {
-      return res.status(400).send('O e-mail já foi cadastrado');
-    }
-
-    // Cria o hash da senha usando bcrypt
+  //Verifica se o e-mail já foi cadastrado      
+    await connection.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, results) => {
+      console.log('verificado os campos')
+      if (err) {
+        console.log('erro com o banco')
+        return res.status(500).send('Erro ao verificar o e-mail no banco de dados');      
+      }
+      if (results.length > 0) {
+        console.log('email cadastrado')
+        return res.status(400).send('O e-mail já foi cadastrado');      
+      }
+    });
     bcrypt.hash(senha, 10, (err, hash) => {
+      console.log('cria o hash')
       if (err) {
         return res.status(500).send('Erro ao criptografar a senha');
       }
-
       // Insere o usuário no banco de dados
-      db.query(
-        'INSERT INTO usuarios (nome, email, senha, nome_usuario) VALUES (?, ?, ?, ?)',
+      connection.execute('INSERT INTO usuarios (nome, email, senha, nomeUsuario) VALUES (?, ?, ?, ?)',
         [nome, email, hash, nomeUsuario],
         (err, results) => {
           if (err) {
+            console.log('erroinsere no banco')
             return res.status(500).send('Erro ao cadastrar o usuário no banco de dados');
           }
+          console.log('cadastro realizado')
           return res.status(200).send('Usuário cadastrado com sucesso');
         }
       );
     });
-  });
 });
+      
+  
 
-//localizar item
+
+//localizar item=====================================================================================================================================
 app.get('/inventario/:id', async (req, res) => {  
   try {
       const connection = await db.connect();
@@ -193,46 +240,6 @@ app.post('/edit/:id', async(req, res) =>{
       res.status(500).send('Ocorreu um erro ao excluir os dados');
     }
   });
-
-//carregando bodyparser com json 
-app.use(bodyparser.json());
-
-app.use(session({
-  secret: 'my-secret-key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-app.get('/minha-rota', (req, res) => {
-  const sessionId = req.session.id; 
-});
-
-//validação
-app.post("/user",[
-    body("username").isLength({min: 5, max: 50}).withMessage("Usuário não está correto!"),
-    body("password").isLength({min: 5, max: 50}).withMessage("Senha não está correta")
-], (req,res)=>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json( {errors: errors.array()} );
-    }
-    res.json({msg: "sucesso"});
-    return response.json(request.body);
-});
-
-app.get("/cadastro", (req, res)=>{
-  res.sendFile(__dirname + "/views/cadastro.html")
-})
-
-
-
-
-
-
-
-
-
-
 
 
 
